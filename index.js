6fa5ce2c96753af5c9a56478a54a306753ee2f16
Ahmed AlAmiri <a.alamiri.96@gmail.com>
2022-04-07 05:58:19 +0300

    Solve task

--- index.js
+++ index.js
@@ -5,41 +5,43 @@ const CATEGORY_FILTER = "categories-list-filter";
 let tasks = [];
 let categories = [];
 
-// REMOVE ME: SAMPLE FILLING
-tasks = [
-  { id: 0, title: "Game of thrones", category: "Movies", done: false },
-  { id: 1, title: "Toy Story 4", category: "Movies", done: false },
-];
-
-categories = ["Movies", "Groceries"];
-// SAMPLE
-renderCategories(categories, CATEGORY_SELECTOR);
-renderCategories(categories, CATEGORY_FILTER);
-renderTasks(tasks, "tasks-list");
-
 function taskChecked(taskId, checked) {
   // implement the delete task.
   // You are given the task id
-  console.log(`${checked ? "" : "UN"}CHECKED TASK`, taskId);
+  const task = tasks.find((task) => task.id === taskId);
+  task.done = checked;
+  renderTasks(tasks, "tasks-list");
 }
 
 function addTask() {
   const selectedCategory = getSelectedCategoryById(CATEGORY_SELECTOR);
   const taskTitle = getNewTaskText();
   // continue the code here
-  alert(`Category: ${selectedCategory} | Task: ${taskTitle}`);
+  tasks.push({
+    id: tasks.length,
+    title: taskTitle,
+    category: selectedCategory,
+    done: false,
+  });
+  renderTasks(tasks, "tasks-list");
 }
 
 function addCategory() {
   const newCategory = getNewCategoryText();
   // continue the code here
-  alert(`New category was added: ${newCategory}`);
+  categories.push(newCategory);
+  renderCategories(categories, CATEGORY_SELECTOR);
+  renderCategories(categories, CATEGORY_FILTER);
 }
 
 function filterTasks() {
   const selectedCategory = getSelectedCategoryById(CATEGORY_FILTER);
   const done = getFilteredDone();
   // continue the code here
-  // REMOVE ME: sample alert
-  alert(`Category: ${selectedCategory} | done: ${done}`);
+  const filteredTasks = tasks.filter((task) =>
+    done
+      ? task.done === true && task.category === selectedCategory
+      : task.category === selectedCategory
+  );
+  renderTasks(filteredTasks, "tasks-list");
 }
